5d71ed34ccc4a93ca198fb6d27c8d503c18d11ff
준혁 김 <kim82544@gmail.com>
2022-06-30 11:51:29 +0900

    220629 백준 벽 부수고 이동하기 다시 풀어보기

--- ConsoleApplication1/2206.cpp
+++ ConsoleApplication1/2206.cpp
@@ -2,14 +2,13 @@
 #include <queue>
 #include <tuple>
 using namespace std;
-#define X first
-#define Y second
 #define MAX 1002
-int board[MAX][MAX];
-int dist[MAX][MAX][2]; // [0] = 벽을 안 부순 최단거리, [1] = 벽을 부순 최단거리
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
+int board[MAX][MAX];
+int dist[MAX][MAX][2]; // [0] = 벽을 부수지 않은 최단거리, [1] = 벽을 부순 최단거리
 int n, m;
+queue <tuple<int, int, int>> q;
 void input()
 {
 	cin >> n >> m;
@@ -19,52 +18,43 @@ void input()
 		for (int j = 0; j < m; j++)
 		{
 			cin >> c;
-			board[i][j] = (c - '0');
+			board[i][j] = c - '0';
 		}
 	}
 }
 int solve()
 {
+	q.push({ 0,0,0 });
+	dist[0][0][0] = 1; // 초기 설정
+	while (!q.empty())
+	{
+		auto cur = q.front(); q.pop();
+		int x, y, broken;
+		tie(x, y, broken) = cur;
+		if (x == n - 1 && y == m - 1) return dist[x][y][broken];
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			dist[i][j][0] = dist[i][j][1] = -1;
-			dist[0][0][0] = dist[0][0][1] = 1;
-			queue <tuple<int, int, int>> q;
-			q.push({ 0,0,0 });
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 			
-			while (!q.empty())
+			if (!broken && board[nx][ny] == 1 && dist[nx][ny][1] == 0) // 벽이 있고,부술 수 있음
 			{
-				int x, y, broken;
-				tie(x, y, broken) = q.front(); q.pop();
-				if (x == n - 1 && y == m - 1) return dist[x][y][broken];
-				int nextdist = dist[x][y][broken] + 1;
-				for (int dir = 0; dir < 4; dir++)
-				{
-					int nx = x + dx[dir];
-					int ny = y + dy[dir];
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-					if (board[nx][ny] == 0 && dist[nx][ny][broken] == -1) // 벽이 아니고, 아직 가지 않은 정점
-					{
-						dist[nx][ny][broken] = nextdist;
-						q.push({ nx,ny,broken });
-					}
-					if (!broken && board[nx][ny] == 1 && dist[nx][ny][1] == -1) // 아직 벽을 부순적 없고, 벽이고,
-																		// 아직 가지 않은 정점
-					{
-						dist[nx][ny][1] = nextdist;
-						q.push({ nx,ny,1 });
-					}
-				}
+				dist[nx][ny][1] = dist[x][y][0] + 1;
+				q.push({ nx,ny,1 });
 			}
-		
-	
-
+			if (board[nx][ny] == 0 && dist[nx][ny][broken] == 0) // 벽은 없지만 갈 수 있는 길
+			{
+				dist[nx][ny][broken] = dist[x][y][broken] + 1;
+				q.push({ nx,ny,broken });
+			}
+		}
+	}
 	return -1;
 }
-int main()	
+int main()
 {
 	cin.tie(0); ios::sync_with_stdio(0);
-	input();
-	cout << solve();
+	input(); cout << solve();
 }
