6c85a173ac0e9f9c29f2432c24798ce5614803cd
준혁 김 <kim82544@gmail.com>
2022-07-12 16:46:29 +0900

    220712 백준 17071 숨바꼭질 5 수정

--- ConsoleApplication1/17071.cpp
+++ ConsoleApplication1/17071.cpp
@@ -23,18 +23,68 @@ int solve()
 	{
 		if (k > 500000) return -1;
 
-		if (ksec % 2 == 0 && even[k] != 0 && brother[k] != 0)
+		if (ksec % 2 == 0 && even[k] != 0 && brother[k] != 0) // 현재 이동한 시간이 짝수
+		{
+			return brother[k];
+		}
+		else if (ksec % 2 == 1 && odd[k] != 0 && brother[k] != 0) // 현재 이동한 시간이 홀수
 		{
 
 			return brother[k];
 		}
-		else if (ksec % 2 == 1 && odd[k] != 0 && brother[k] != 0)
+		
+		int t = q.size(); 
+		while (t--) // 수빈 짝수/홀수로 나누기
 		{
+			auto cur = q.front(); q.pop();
+			int x, sec;
+			x = cur.first; sec = cur.second;
+			sec++; // 여기서 1 증가시켜주는 이유는 if문장의 조건을 만족하기 위해서
+					
+			if (sec % 2 == 1) // 위에서 증가시켜야 1초일때 홀수, 2초일때 짝수... 배열로 들어감
 
-			return brother[k];
+			{
+				if (x > 0 && odd[x - 1] == 0)
+				{
+					odd[x - 1] = sec;
+					q.push({ x - 1,sec });
+
+				}
+				if (x < 500000 && odd[x + 1] == 0)
+				{
+					odd[x + 1] = sec;
+					q.push({ x + 1,sec });
+				}
+				if (x > 0 && x * 2 <= 500000 && odd[x * 2] == 0)
+				{
+					odd[x * 2] = sec;
+					q.push({ x * 2,sec });
+				}
+			}
+			else
+			{
+				if (x > 0 && even[x - 1] == 0)
+				{
+					even[x - 1] = sec;
+					q.push({ x - 1,sec });
+
+				}
+				if (x < 500000 && even[x + 1] == 0)
+				{
+					even[x + 1] = sec;
+					q.push({ x + 1,sec });
+				}
+				if (x > 0 && x * 2 <= 500000 && even[x * 2] == 0)
+				{
+					even[x * 2] = sec;
+					q.push({ x * 2,sec });
+				}
+			}
+
 		}
-		int t = sub.size();
-		while (t--)
+
+		t = sub.size();
+		while (t--) // 동생 x+sec초 증가
 		{
 
 			auto cur = sub.front(); sub.pop();
@@ -49,56 +99,6 @@ int solve()
 			}
 
 		}
-		t = q.size();
-		while (t--)
-		{
-			auto cur = q.front(); q.pop();
-			int x, sec;
-			x = cur.first; sec = cur.second;
-
-			if (sec % 2 == 0)
-
-			{
-				if (x > 0 && odd[x - 1] == 0)
-				{
-					odd[x - 1] = sec + 1;
-					q.push({ x - 1,sec + 1 });
-
-				}
-				if (x < 500000 && odd[x + 1] == 0)
-				{
-					odd[x + 1] = sec + 1;
-					q.push({ x + 1,sec + 1 });
-				}
-				if (x > 0 && x * 2 <= 500000 && odd[x * 2] == 0)
-				{
-					odd[x * 2] = sec + 1;
-					q.push({ x * 2,sec + 1 });
-				}
-			}
-			else
-			{
-				if (x > 0 && even[x - 1] == 0)
-				{
-					even[x - 1] = sec + 1;
-					q.push({ x - 1,sec + 1 });
-
-				}
-				if (x < 500000 && even[x + 1] == 0)
-				{
-					even[x + 1] = sec + 1;
-					q.push({ x + 1,sec + 1 });
-				}
-				if (x > 0 && x * 2 <= 500000 && even[x * 2] == 0)
-				{
-					even[x * 2] = sec + 1;
-					q.push({ x * 2,sec + 1 });
-				}
-			}
-
-		}
-
-
 
 
 
