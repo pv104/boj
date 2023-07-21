e4289e93d553cf258fb9a265418a184a5fddc377
pv104 <71826117+pv104@users.noreply.github.com>
2023-07-21 14:01:05 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,68 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define MAX 25
-#define END first.first
-#define START first.second
+#define START first
 #define WEIGHT second
+#define MAX 25
 using namespace std;
-int dp[MAX];
-vector <pair<pair<int,int>,int>> v[MAX]; // <<종료,시작>,돈>
+vector <pair<int,int>> v[MAX]; // <시작,돈>, 작업이 끝나는 날짜가 인덱스
+vector <int> dp(MAX); // dp[i] = dp[1]~dp[i-1]까지의 최대 이익
 int n, t, p;
 void input()
 {
 	cin >> n;
-	v[0].push_back({ {0,0},0 });
 	for (int i = 1; i <= n; i++)
 	{
-		v[i].push_back({ {0,0},0 });
-		bool flag = false;
 		cin >> t >> p;
-		
 		int ED = t + i - 1;
-		
-		v[ED].push_back({{ED,i},p});
+		v[ED].push_back({ i,p });
 	}
-	for (int i=1; i<=n; i++)
-	sort(v[i].begin(), v[i].end());
 }
 int solve()
 {
-	int res = 0;
-	if(v[1].size() > 1)
-	dp[1] = v[1][1].WEIGHT;
 	for (int i = 1; i <= n; i++)
 	{
-
-		for (int j = 0; j < i; j++)
-		{
 			for (auto& k : v[i])
 			{
-				if (k.START <= j) continue;
-				
-					dp[i] = max(dp[i], dp[j] + k.WEIGHT);
-				
-
+				// 직전 값(현재 최대값)의 인덱스를 찾음
+				auto it = lower_bound(dp.begin(), dp.begin() + i, dp[i - 1]);
+				auto idx = it - dp.begin();
+				// 인덱스보다 i날의 작업 시작일이 큰 경우에는 추가할 수 있음
+				if (idx < k.START)
+					dp[i] = max(dp[i], dp[idx] + k.WEIGHT);
+				// 인덱스보다 i날의 작업 시작일이 같거나 작은 경우에는 추가할 수 없음
+				// dp[idx]에 작업이 끝나고 바로 시작할 수 없고, 
+				// 작업 시작일이 더 작은 경우에는 해당 작업을 선택하는게 손해
+				else
+					dp[i] = max(dp[i], max(dp[idx],dp[k.START - 1] + k.WEIGHT));
 			}
-		}
-		if (dp[i] == 0)
-		{
-			for (auto k : v[i])
-				dp[i] = max(dp[i], k.WEIGHT);
-		}
+			// 해당 작업일에 종료되는 작업이 없는 경우
+		if(dp[i] == 0)
+		dp[i] = dp[i-1];
 	}
-	
 
-	for (int i = 1; i <= n; i++)
-		res = max(dp[i], res);
-	return res;
+	return dp[n];
 }
 int main()
 {
 	cin.tie(0); ios::sync_with_stdio(0);
 	input();
-
 	cout << solve();
-
 	return 0;
 }
