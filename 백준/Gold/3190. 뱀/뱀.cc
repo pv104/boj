d5122aef1359206903e3d167c4cb4d204a437360
pv104 <71826117+pv104@users.noreply.github.com>
2024-02-01 17:51:30 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,105 +1,81 @@
 #include <iostream>
-#include <algorithm>
 #include <deque>
+#include <queue>
 #include <vector>
-#define MAX 103
-#define X first
-#define Y second
 using namespace std;
-int idx, dir, n, k, l, now, sec;
-vector <pair<int, int>> v;
-deque <pair<int, int>> dq;
-int board[MAX][MAX];
-int move()
-{
-	auto cur = dq.front();
-	int x = cur.X;
-	int y = cur.Y;
-	if (dir == 0)
-	{
-		x += -1;
-	}
-	else if (dir == 1)
-	{
-		y += 1;
-	}
-	else if (dir == 2)
-	{
-		x += 1;
-	}
-	else
-		y += -1;
-	if (x < 1 || y < 1 || x > n || y > n) return 1;
-	for (auto cur = dq.begin() + 1; cur != dq.end(); cur++)
-	{
-		if (x == cur->X && y == cur->Y) return 1;
-	}
-	if (board[x][y])
-	{
-		board[x][y] = 0;
-	}
-	else
-		dq.pop_back();
-	dq.push_front({x,y});
-	return 0;
-}
-void input()
-{
-	int x, y;
-	int r;
-	char d;
-	dq.push_front({ 1,1 });
-	dir = 1;
-	cin >> n;
-	cin >> k;
-	for (int i = 0; i < k; i++)
-	{
-		cin >> x >> y;
-		board[x][y] = 1;
+#define MAX 102
+int n, k, l;
+int curR = 0;
+queue <pair<int, char>> R; // 회전
+deque <pair<int, int>> S; // 뱀
+
+char board[MAX][MAX];
+void input() {
+	cin >> n >> k;
+	int r, c;
+	S.push_back({ 1,1 });
+	for (int i = 0; i < k; i++) {
+		cin >> r >> c;
+		board[r][c] = 'A';
 	}
 	cin >> l;
-	for (int i = 0; i < l; i++)
-	{
-		cin >> r >> d;
-		if (d == 'L')
-			v.push_back({ r,0 });
-		else
-			v.push_back({ r,1 });
+	int t;
+	char x;
+	for (int i = 0; i < l; i++) {
+		cin >> t >> x;
+		R.push({ t,x });
 	}
 }
-void rotate(int direction)
-{
-	if (direction == 0) // 왼쪽
-	{
-		if (dir == 0)
-			dir = 3;
-		else dir--;
-	}
-	else // 오른쪽
-	{
-		if (dir == 3)
-			dir = 0;
-		else dir++;
-	}
-}
-int solve()
-{
-	while (true)
-	{
-		if (move() > 0) return sec + 1;
-		else sec++;
-		if (idx < v.size())
-		{
-			if (v[idx].X == sec)
-			{
-				rotate(v[idx++].Y);
-			}
+int solve() {
+	int res = 0;
+	int nx = 1;
+	int ny = 1;
+	for (int timer = 1; ; timer++) {
+		if (curR == 0) {
+			ny++;
 		}
+		else if (curR == 1) {
+			nx++;
+		}
+		else if (curR == 2) {
+			ny--;
+		}
+		else if (curR == 3) {
+			nx--;
+		}
+		S.push_back({ nx,ny }); // 뱀 길이 늘리기
+		
+		
+		if (nx < 1 || ny < 1 || nx > n || ny > n) { // 벽
+			return timer;
+		}
+		for (int i = 0; i < S.size() - 1; i++) {
+			if (S[i].first == nx && S[i].second == ny) return timer;
+		
+		}
+		
+		if (board[nx][ny] != 'A') {
+			S.pop_front();
+		}
+		else {
+			board[nx][ny] = ' ';
+		}
+		if (!R.empty() && (R.front().first == timer)) { // 방향 변환
+			if (R.front().second == 'D') {
+				curR++;
+			}
+			else {
+				curR--;
+			}
+			if (curR < 0) curR = 3;
+			if (curR > 3) curR = 0;
+			R.pop();
+		}
+		
 	}
+	return res;
 }
-
-int main()
-{
+int main() {
 	cin.tie(0); ios::sync_with_stdio(0);
 	input();
 	cout << solve();
