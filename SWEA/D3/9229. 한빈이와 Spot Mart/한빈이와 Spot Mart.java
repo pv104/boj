c46eb2a0433407ed2833a045d03e0137952c7391
pv104 <71826117+pv104@users.noreply.github.com>
2024-02-06 17:45:21 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 159 ms, Memory: 29,428 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -5,34 +5,28 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	final int MAX = 1003;
-	static int dp[][];
 	static int snack[];
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static int solve() throws IOException {
+		int res = -1;
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		snack = new int[n+1];
-		dp = new int[n+1][n+1];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= n; i++)
 			snack[i] = Integer.parseInt(st.nextToken());
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (i == j) continue;
-				dp[i][j] = snack[i] + snack[j];
-				if (dp[i][j] > m) dp[i][j] = -1;
-				
+				int t = snack[i]+snack[j];
+				if(t > m) continue;
+				if (t == m) return t;
+				res = Math.max(t,res);
 			}
 		}
-		int res = -1;
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= n; j++)
-				res = Math.max(res, dp[i][j]);
-			}
 		if(res == 0) res = -1;
 		return res;
 	}
