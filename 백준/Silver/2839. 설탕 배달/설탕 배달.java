92f99debee9a219e6cb413289e7c405acdb38639
pv104 <71826117+pv104@users.noreply.github.com>
2024-02-13 16:06:34 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,18 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 public class Main {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static final int INF = 987654321;
-	static final int RES = -1;
-	static int dp[],n;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		n = Integer.parseInt(br.readLine());
-		dp = new int[n+10];
-		Arrays.fill(dp, INF);
-		dp[3] = dp[5] = 1;
-		dp[6] = 2;
-		for(int i=8; i<=n; i++) {
-			dp[i] = Math.min(dp[i-3]+1, dp[i-5]+1);
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		int cnt=0;
+		if(n == 4 || n == 7) System.out.print(-1);
+		else {
+			while(n % 5 != 0) {
+				++cnt;
+				n-=3;
+			}
+			System.out.print(cnt + (n/5));
 		}
-		if(dp[n] == INF) dp[n] = RES;
-		
-		System.out.print(dp[n]);
 	}
 }
