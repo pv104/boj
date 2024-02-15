48cd74bc6468347dc185056f10c05dc59051381e
pv104 <71826117+pv104@users.noreply.github.com>
2024-02-15 10:12:42 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold III/14890. 경사로/경사로.cc
+++ 백준/Gold III/14890. 경사로/경사로.cc
@@ -1,45 +1,5 @@
-//#include <iostream>
-//#include <queue>
-//#define W first.first
-//#define X first.second
-//#define Y second
-//using namespace std;
-//const int MAX = 1003;
-//const int INF = 987654321;
-//int n, m, x;
-//vector <vector<pair<int,int>>> board;
-//queue <pair<pair<int,int>,int>> q;
-//int startCost[MAX], endCost[MAX];
-//void input() {
-//	cin >> n >> m >> x;
-//	int r, c, w;
-//	board.resize(n);
-//	for (int i = 0; i < n; i++) {
-//		cin >> r >> c >> w;
-//		board[r].push_back({ w,c }); // r->c에 w
-//	}
-//}
-//int solve() {
-//	int res = 0;
-//	fill(startCost, startCost + MAX, INF);
-//	fill(endCost, endCost + MAX, INF);
-//	startCost[1] = 0;
-//	for (int i = 1; i <= n; i++) {
-//		for (int j = 1; j <= n; j++) {
-//			if (board[i].W < INF) {
-//
-//			}
-//		}
-//	}
-//}
-//int main() {
-//	cin.tie(0); ios::sync_with_stdio(0);
-//}
-
-
 #include <iostream>
 #include <vector>
-#include <stack>
 using namespace std;
 int n, l;
 const int MAX = 102;
@@ -60,7 +20,6 @@ int isAble(vector<int> arr) {
 		if (abs(diff) > 1) { // 불가능
 			return 0;
 		}
-		
 		if (diff == 1) { // i>i+1
 			int cnt = 1;
 			for (int j = i + 1; j < size - 1; j++) {
@@ -90,8 +49,6 @@ int isAble(vector<int> arr) {
 					else return 0;
 				}
 			}
-			else if (i == 0 && l == 1)
-				w[i] = true;
 			else return 0;
 		}
 	}
