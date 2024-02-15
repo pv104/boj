ce8a4219b144962f39a2b7237bac57e503cfd37d
pv104 <71826117+pv104@users.noreply.github.com>
2024-02-15 10:29:09 +0900

    [Gold III] Title: 경사로, Time: 80 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Gold III/14890. 경사로/경사로.java
+++ 백준/Gold III/14890. 경사로/경사로.java
@@ -15,6 +15,7 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			String s = br.readLine();
 			int len = s.length();
+			/** 입력이 (한자리숫자' '한자리숫자' ' 형태이므로)*/
 			for (int j = 0, index = 0; index < len && j < n; j++, index+=2) {
 				board[i][j] = s.charAt(index);
 			}
@@ -26,24 +27,25 @@ public class Main {
 
 		boolean w[] = new boolean [size+1];
 		for (int i = 0; i < size; i++) {
+			/** 차이를 계산(1이거나 0이거나 -1이거나, 이외는 안 됨*/
 			int diff = arr[i] - arr[i + 1];
-			if (Math.abs(diff) > 1) { // 불가능
+			if (Math.abs(diff) > 1) { // 절댓값이 1 초과 = 불가능
 				return 0;
 			}
-			if (diff == 1) { // i>i+1
-				int cnt = 1;
+			if (diff == 1) { // i>i+1 5>4처럼 앞쪽이 더 큰 경우
+				int cnt = 1; // ~5까지 무시하고, 뒤쪽에 경사로 놓을 수 있는지 체크. 1칸은 기본적으로 가능
 				for (int j = i + 1; j < size; j++) {
-					if (arr[j] == arr[j + 1]) ++cnt;
+					if (arr[j] == arr[j + 1]) ++cnt; // 같아야 놓을 수 있음
 					else break;
 				}
-				if (cnt >= l) {
-					int target = i + 1 + l;
+				if (cnt >= l) { // l개 이상이면 놓으면 됨(꼭 l개가 아니어도 되니까)
+					int target = i + 1 + l; // 놓을 경사로의 인덱스
 					for (int j = i + 1; j < size && j < target; j++) {
-						if (w[j] == false)
+						if (w[j] == false) // 아직 경사로가 놓여있지 않은 경우
 							w[j] = true;
 						else return 0;
 					}
-					i += cnt-1;
+					i += cnt-1; // 다음 i로 이동하기
 				}
 				else return 0;
 			}
@@ -70,6 +72,7 @@ public class Main {
 
 	static int solve() throws Exception{
 		int res = 0;
+		// 배열을 row 1줄, column 1줄씩 각각 옮겨서 체크(독립적)
 		for (int i = 0; i < n; i++) {
 			int[] t = new int[n];
 			for (int j = 0; j < n; j++) 
