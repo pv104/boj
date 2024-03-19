9e0a268db39889ff2ff8bb8fcb905df0941b6cc2
pv104 <71826117+pv104@users.noreply.github.com>
2024-03-19 11:33:53 +0900

    [Gold V] Title: 내려가기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -1,35 +1,24 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n;
 #define MAX 3
 #define INF 987654321
-int dp_min[MAX], dp_max[MAX], t_min[MAX], t_max[MAX];
-int arr[MAX];
+int dp_min[MAX], dp_max[MAX], t_min[MAX], t_max[MAX], arr[MAX], n;
 int delta[MAX] = { -1,0,1 };
 void input() {
 	cin >> n;
-	for (int j = 0; j < MAX; j++)
-	{
-		cin >> arr[j];
-		dp_min[j] = dp_max[j] = arr[j];
-		t_min[j] = INF;
-		t_max[j] = -1;
+	for (int i = 0; i < MAX; i++) {
+		t_min[i] = INF;
+		t_max[i] = -1;
 	}
 }
 pair<int,int> solve() {
-	for (int i = 1; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 
 		for (int j = 0; j < MAX; j++)
 			cin >> arr[j];
 		for (int j = 0; j < MAX; j++)
 		{
-			int minValue = INF;
-			int maxValue = -1;
-			int nxt = 0;
-			int nxt_min = 0;
-
-			// j번째 dp값과 arr[cur]를 비교해서 가장 큰 값으로 이동하기
 			for (int k = 0; k < MAX; k++) {
 				int cur = j + delta[k];
 				if (cur < 0 || cur >= MAX) continue;
@@ -50,7 +39,6 @@ pair<int,int> solve() {
 	for (int i = 0; i < MAX; i++) {
 		res.first = max(dp_max[i], res.first);
 		res.second = min(dp_min[i],res.second);
-		
 	}
 	return res;
 }
