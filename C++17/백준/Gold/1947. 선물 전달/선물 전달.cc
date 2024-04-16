ea1ab97d0184a1a9c99962aa46b643d7498a08e9
pv104 <71826117+pv104@users.noreply.github.com>
2024-04-16 14:42:45 +0900

    [Gold III] Title: 선물 전달, Time: 12 ms, Memory: 9832 KB -BaekjoonHub

--- C++17/백준/Gold/1947. 선물 전달/선물 전달.cc
+++ C++17/백준/Gold/1947. 선물 전달/선물 전달.cc
@@ -1,25 +1,15 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <memory.h>
 using namespace std;
 #define MAX 1000002
 #define ll long long
 #define MOD (ll)1e9
 ll dp[MAX], N;
-
-void input() {
-	cin >> N;
-}
-ll solve() {
-	dp[2] = 1;
-	dp[3] = 2;
-	for (int i = 4; i <= N; i++) {
-		dp[i] = ((((dp[i - 2] + dp[i - 1]) % MOD) * (i - 1)) % MOD);
-	}
-	return dp[N];
-}
 int main() {
 	cin.tie(0); ios::sync_with_stdio(0);
-	input(); cout << solve();
+	cin >> N; 
+	dp[2] = 1;
+	for (int i = 3; i <= N; i++) 
+	dp[i] = ((((dp[i - 2] + dp[i - 1]) % MOD) * (i - 1)) % MOD);
+	cout << dp[N];
 }
