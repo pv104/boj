5f54c8bc874b5ab31652ea4959f15977548e6595
pv104 <71826117+pv104@users.noreply.github.com>
2024-04-20 16:03:14 +0900

    [Silver I] Title: 단절점과 단절선, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 #define MAX 100002
-int N, M, A, B, Q, K, T;
+int N, A, B, Q, K, T;
 int arr[MAX];
 void input() {
 	cin >> N;
 	--N;
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; ++i) {
 		cin >> A >> B;
 		++arr[A];
 		++arr[B];
@@ -16,15 +15,16 @@ void input() {
 }
 void solve() {
 
-	for (int i = 0; i < Q; i++) {
+	for (int i = 0; i < Q; ++i) {
 		cin >> T >> K;
 		if (T == 1) {
-			if (arr[K] == 1) cout << "no" << "\n";
-			else if (arr[K] > 1) cout << "yes" << "\n";
-		}
-		else
+			if (arr[K] == 1) {
+                cout << "no" << "\n";
+                continue;
+                }	
+		    }
 			cout << "yes" << "\n";
-	}
+	    }
 }
 int main() {
 	cin.tie(0); ios::sync_with_stdio(0);
