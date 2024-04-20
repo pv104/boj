496948d3a72e8f36744b93bd0a22984317a1d65f
pv104 <71826117+pv104@users.noreply.github.com>
2024-04-20 16:50:45 +0900

    [Silver I] Title: 단절점과 단절선, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
@@ -1,2 +1,2 @@
 #include<iostream>
-using namespace std;int a[100001];int main(){int N,A,B;cin.tie(0);cin.sync_with_stdio(0);cin>>N;while(--N){cin>>A>>B;++a[A];++a[B];}cin>>N;while(N--){cin>>A>>B;(A==1&&a[B]==1)?cout<<"no":cout<<"yes";cout<<"\n";}}
+using namespace std;int a[100001];int main(){int N,A,B;cin.tie(0);cin.sync_with_stdio(0);cin>>N;while(--N){cin>>A>>B;++a[A];++a[B];}cin>>N;while(N--){cin>>A>>B;((A|a[B])==1)?cout<<"no":cout<<"yes";cout<<"\n";}}
