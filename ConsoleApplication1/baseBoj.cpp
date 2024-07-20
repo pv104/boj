21387c8b8b0144a2caf36c17cc1ae76ab99fb243
pv104 <pv104@naver.com>
2024-07-20 10:18:57 +0900

    basecpp add

---
+++ ConsoleApplication1/baseBoj.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <map>
+#include <set>
+#include <memory.h>
+using namespace std;
+#define fileio FILE* stream; freopen_s(&stream,"input.txt","r",stdin)
+#define fastio cin.tie(0); cin.sync_with_stdio(0)
+#define SIZE 100
+#define INF 1987654321
+int N, T;
+void input() {
+
+}
+int solve() {
+	int res = 0;
+	return res;
+}
+int main() {
+	fastio;
+	fileio;
+	cout << solve();
+}
---
+++ ConsoleApplication1/baseSWEA.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <map>
+#include <set>
+#include <memory.h>
+using namespace std;
+#define fileio FILE* stream; freopen_s(&stream,"input.txt","r",stdin)
+#define fastio cin.tie(0); cin.sync_with_stdio(0)
+#define SIZE 100
+#define INF 1987654321
+int N, T;
+void init() {
+
+}
+void input() {
+
+}
+int solve() {
+	int res = 0;
+	return res;
+}
+int main() {
+	fastio;
+	fileio;
+	cin >> T;
+	for (int i = 1; i <= T; i++)
+	{
+		init();
+		input();
+		cout << "#" << i << " " << solve() << "\n";
+	}
+}
