6f2ae8374bb0791300fc0a60c1d1b32cc9f301c4
JunHyeok Kim <71826117+pv104@users.noreply.github.com>
2025-03-14 15:23:36 +0900

    [Gold III] Title: 산책 (small), Time: 16 ms, Memory: 4192 KB -BaekjoonHub

--- 백준/Gold/22868. 산책 （small）/산책 （small）.cc
+++ 백준/Gold/22868. 산책 （small）/산책 （small）.cc
@@ -5,17 +5,13 @@
 #include <map>
 #include <memory.h>
 #define N_SIZE 10003
-#define fileio FILE* stream; freopen_s(&stream, "input.txt", "r", stdin)
-#define START 0
-#define END 1
 using namespace std;
-int N, M, S, E, minDist;
+int N, M, S, E;
 vector <int> v[N_SIZE];
 bool visited[N_SIZE];
 map <int, int> m[N_SIZE];
 queue <pair<int,int>> q;
 void input() {
-    minDist = 1987654321;
     memset(visited, false, sizeof(visited));
     cin >> N >> M;
     int r, c;
@@ -28,8 +24,8 @@ void input() {
     for (int i = 0; i < N; i++) {
         sort(v[i].begin(), v[i].end());
     }
+
 }
- //직전 정점 저장해두기. <cur,pre>
 int bfs(int st,int ed) {
     q.push({ st,0 });
     int dist = 0;
@@ -39,19 +35,8 @@ int bfs(int st,int ed) {
             --size;
             auto cur = q.front(); q.pop();
             int val = cur.first;
-            int pre = cur.second;
             // 루트 찾기
             if (val == ed) {
-                memset(visited, false, sizeof(visited));
-                int cur = val;
-                // 방문 처리
-                for (int i = dist - 1; i > 0; i--) {
-                    auto nxt = m[i].find(cur);
-                    cur = nxt->second;
-                    visited[cur] = true;
-                    //cout << " cur : " << cur << "\n";
-                }
-                while (!q.empty()) q.pop();
                 return dist;
             }
             for (auto nxt : v[val]) {
@@ -59,7 +44,6 @@ int bfs(int st,int ed) {
                 visited[nxt] = true;
                 q.push({ nxt,val });
                 m[dist].insert({ nxt,val });
-                //cout << " dist : " << dist << " nxt : " << nxt << " pre : " << val << "\n";
             }
         }
         dist++;
@@ -68,21 +52,23 @@ int bfs(int st,int ed) {
 int solve() {
     visited[S] = true;
     int res = bfs(S, E);
-
-    for (auto i : m) {
+    while (!q.empty()) q.pop();
+    for (auto i : m)
         i.clear();
+    memset(visited, false, sizeof(visited));
+    int cur = E;
+    for (int i = res - 1; i > 0; i--) {
+        auto nxt = m[i].find(cur);
+        cur = nxt->second;
+        visited[cur] = true;
     }
-
     visited[E] = true;
-
     return res + bfs(E, S);
 
 }
 int main() {
-    //fileio;
     cin.tie(0);
     cin.sync_with_stdio(0);
-
     input();
     cout << solve();
 }
