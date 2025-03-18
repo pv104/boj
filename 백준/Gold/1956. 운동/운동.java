ca938fb4d3825fd9510ed010951a5d38a02ecb9c
JunHyeok Kim <71826117+pv104@users.noreply.github.com>
2025-03-18 12:11:27 +0900

    [Gold IV] Title: 운동, Time: 476 ms, Memory: 58868 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int INF = 987654321,SIZE = 403;
+    public static int INF = 987654321,SIZE = 401;
     public static int V,E;
     public static int[][] arr = new int[SIZE][SIZE];
     public static int[][] dist = new int[SIZE][SIZE];
@@ -17,17 +17,15 @@ public class Main {
     }
     public static void input() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        V = Integer.parseInt(str.split(" ")[0]);
-        E = Integer.parseInt(str.split(" ")[1]);
-//        System.out.println(" V : " + V + " E : " + E);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
         for (int i = 0; i < E; i++) {
-            str = br.readLine();
-            int r = Integer.parseInt(str.split(" ")[0]);
-            int c = Integer.parseInt(str.split(" ")[1]);
-            arr[r][c] = Integer.parseInt(str.split(" ")[2]);
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr[r][c] = Integer.parseInt(st.nextToken());
             dist[r][c] = arr[r][c];
-//            System.out.println(" R : " + r + " C : " + c + " arr[r][c] : " + arr[r][c]);
         }
     }
     public static void floyd() {
@@ -45,9 +43,8 @@ public class Main {
     public static int solve() {
         int res = INF;
         for(int i=1; i<=V; i++) {
-            for(int j=1; j<=V; j++) {
+            for(int j=i+1; j<=V; j++) {
                 if(i==j) continue;
-//                System.out.println("dist[i][j] : " + dist[i][j] + "dist[j][i] : " + dist[j][i]);
                 res = Math.min(res,dist[i][j]+dist[j][i]);
             }
         }
@@ -56,7 +53,6 @@ public class Main {
     }
     public static void main(String[] args) throws Exception {
         init();
-
         input();
         floyd();
         System.out.println(solve());
