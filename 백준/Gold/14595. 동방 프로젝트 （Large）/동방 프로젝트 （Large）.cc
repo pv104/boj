2f6395fa7929c37207eebb01bceab2054e653f3c
JunHyeok Kim <71826117+pv104@users.noreply.github.com>
2025-04-04 15:16:20 +0900

    [Gold IV] Title: 동방 프로젝트 (Large), Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/14595. 동방 프로젝트 （Large）/동방 프로젝트 （Large）.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+#define SIZE 2003
+struct cmp {
+    bool operator() (int a, int b) const&{
+        
+        if (a == b) return false;
+        return a < b;
+    }
+};
+map <int, int, cmp> m, range;
+int N, M;
+void input() {
+    cin >> N >> M;
+    int k, v;
+    for (int i = 0; i < M; i++) {
+        cin >> k >> v;
+        if (m.find(k) != m.end() && m.find(k)->second < v)
+            m.insert_or_assign( k, v );
+        else
+            m.insert({ k, v });
+    }
+}
+int solve() {
+    if (M == 0) return N;
+    int l = m.begin()->first, r = m.begin()->second;
+    // l,r 계산
+    for (auto cur : m) {
+        int k = cur.first, v = cur.second;
+        if (k > r) {
+            range.insert({ l, r });
+            l = k;
+            r = v;
+        }
+        l = min(l, k);
+        r = max(r, v);
+        
+    }
+    range.insert({ l, r });
+    int res = N;
+    for (auto cur : range) {
+        res -= (cur.second - cur.first);
+    }
+    return res;
+}
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    input();
+    cout << solve();
+}
