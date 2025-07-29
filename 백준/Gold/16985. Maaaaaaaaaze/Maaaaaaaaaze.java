3730f2b11e6d76f7a5c7348ceb384d541b9cc719
JunHyeok Kim <71826117+pv104@users.noreply.github.com>
2025-07-29 17:04:04 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 1028 ms, Memory: 75148 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
@@ -19,10 +19,11 @@ public class Main {
 
 	static void pmt(int k) {
 
+		if (answer == 12)
+			return;
 		if (k == N) {
-//			System.out.print("[pmt] k == N");
-			{			copy();
-			pmtR(0);}
+			copy();
+			pmtR(0);
 			return;
 		}
 		for (int i = 0; i < N; i++) {
@@ -36,22 +37,16 @@ public class Main {
 	}
 
 	static void copy() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				for (int k = 0; k < N; k++)
 					cA[num[i]][j][k] = A[i][j][k];
-				}
-			}
-		}
 	}
 
 	static void pmtR(int k) {
+		if (answer == 12)
+			return;
 		if (k == N) {
-//			System.out.println("[pmtR], k == n");
-//			System.out.print("[pmtR]");
-//			for (int i=0; i<5; i++)
-//				System.out.print(" numR[i]: " + numR[i]);
-//			System.out.println();
 			run();
 			return;
 		}
@@ -66,24 +61,22 @@ public class Main {
 	static void bfs() {
 		q = new ArrayDeque<>();
 		int[] dest = new int[3], src = new int[3];
-		// {0,1} / {2,3} / {4,5} / {6,7} bfs
 
 		for (int cb = 0; cb < 8; cb += 2) {
 			for (int k = 0; k < 3; k++) {
 				dest[k] = dots[cb][k];
-				src[k] = dots[cb + 1][k]; // 꼭지점 설정
+				src[k] = dots[cb + 1][k];
 			}
 			for (int i = 0; i < 5; i++)
 				for (int j = 0; j < 5; j++)
 					for (int k = 0; k < 5; k++)
-						visited[i][j][k] = false; // 초기화
+						visited[i][j][k] = false;
 			int s = dest[0];
 			int r = dest[1];
 			int c = dest[2];
 			int p = 0;
-//			System.out.print(" dst => " + dest[0] + " " + dest[1] + " " + dest[2]);
-//			System.out.println(" src => " + src[0] + " " + src[1] + " " + src[2]);
-			if(cA[s][r][c] == 0 || cA[src[0]][src[1]][src[2]] == 0) continue;
+			if (cA[s][r][c] == 0 || cA[src[0]][src[1]][src[2]] == 0)
+				continue;
 			q.add(new int[] { s, r, c, p });
 			visited[s][r][c] = true;
 			// 초기 설정
@@ -98,55 +91,45 @@ public class Main {
 					break;
 				}
 				if (s == src[0] && r == src[1] && c == src[2]) {
-//					System.out.print(" dst => " + dest[0] + " " + dest[1] + " " + dest[2]);
-//					System.out.print(" src => " + src[0] + " " + src[1] + " " + src[2]);
-//					System.out.println(" s, r, c => " + s + " " + r + " " + c + " " + " p : " + p);
 					answer = Math.min(answer, p);
 					q.clear();
 					break;
 				}
 				for (int dir = 0; dir < 6; dir++) {
-					int ns = s+dt[dir][0];
-					int nr = r+dt[dir][1];
-					int nc = c+dt[dir][2];
-					if(s == 0) {
-					if(nr < 0 || nc < 0 || nr > 4 || nc > 4 || ns < 0 || ns > 4 || visited[ns][nr][nc]) continue;
-					if(cA[ns][nr][nc] == 0) continue;
-					visited[ns][nr][nc] = true;
-					q.add(new int[] {ns,nr,nc,p+1});
-					} else {
-						if(nr < 0 || nc < 0 || nr > 4 || nc > 4 || ns < 0 || ns > 4 || visited[ns][nr][nc]) continue;
-						if(cA[ns][nr][nc] == 0) continue;
+					int ns = s + dt[dir][0];
+					int nr = r + dt[dir][1];
+					int nc = c + dt[dir][2];
+					if (s == 0) {
+						if (nr < 0 || nc < 0 || nr > 4 || nc > 4 || ns < 0 || ns > 4 || visited[ns][nr][nc])
+							continue;
+						if (cA[ns][nr][nc] == 0)
+							continue;
 						visited[ns][nr][nc] = true;
-						q.add(new int[] {ns,nr,nc,p+1});
+						q.add(new int[] { ns, nr, nc, p + 1 });
+					} else {
+						if (nr < 0 || nc < 0 || nr > 4 || nc > 4 || ns < 0 || ns > 4 || visited[ns][nr][nc])
+							continue;
+						if (cA[ns][nr][nc] == 0)
+							continue;
+						visited[ns][nr][nc] = true;
+						q.add(new int[] { ns, nr, nc, p + 1 });
 					}
 				}
 			}
 
 		}
-		// bfs 4방향만
-
 	}
 
 	static void run() {
-		// 판 결정하기
-		// 회전하기
-		// bfs 수행하기
-
-//		System.out.println("rotate 전");
-//		print();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < numR[i]; j++) {
-//				System.out.println("rotate 전");
-//				print();
 				rotate(num[i]); // 회전하기
-//				System.out.println("rotate 후");
-//				print();
 			}
 		}
 		bfs();
 		copy();
 	}
+
 	static void rotate(int i) {
 		for (int j = 0; j < N; j++) {
 			for (int k = 0; k < N; k++) {
@@ -158,33 +141,21 @@ public class Main {
 				cA[i][j][k] = tA[i][j][k];
 	}
 
-	static void print() {
-		for (int i=0; i<N; i++) {
-			System.out.println(" i : " + i);
-			for (int j=0; j<N; j++) {
-				for (int k=0; k<N; k++) {
-					if(cA[i][j][k] == 1)
-					System.out.print(" i, j, k => " + i + " " + j + " " + k);
-				}
-			}
-			System.out.println();
-		}
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br;
 		br = new BufferedReader(new InputStreamReader(System.in));
-//		br = new BufferedReader(new FileReader("input.txt"));
-		
+
 		StringTokenizer st;
-		for (int i=0; i<N; i++) {
-			for (int j=0; j<N; j++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				st = new StringTokenizer(br.readLine());
-				for (int k=0; k<N; k++)
+				for (int k = 0; k < N; k++)
 					A[i][j][k] = Integer.parseInt(st.nextToken());
 			}
 		}
 		pmt(0);
-		if(answer == Integer.MAX_VALUE) answer = -1;
+		if (answer == Integer.MAX_VALUE)
+			answer = -1;
 		System.out.print(answer);
 	}
 }
