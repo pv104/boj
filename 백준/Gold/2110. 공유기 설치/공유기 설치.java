25303c700a0c8747c3eeee8ff648fd7347809cc2
JunHyeok Kim <71826117+pv104@users.noreply.github.com>
2026-03-31 16:58:19 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 41092 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -4,25 +4,21 @@ import java.util.*;
 public class Main {
 
 	public static int N,C;
-	public static ArrayList<Integer> a = new ArrayList<>();
+	public static int[] a;
 	public static boolean chk(int d) {
-		int cnt=1,cur=a.get(0);
+		int cnt=1,cur=a[0];
 		for (int i=1; i<N; i++) {
-			if(cur+d<=a.get(i)) {
+			if(a[i] - cur >= d) {
 				cnt++;
-				cur = a.get(i);
+				cur = a[i];
 			}
 		}
 		return cnt >= C;
 	}
 	public static int solve() {
-		int lo=a.get(1) - a.get(0), hi = a.get(N-1) - a.get(0);
-		for (int i=1; i<N; i++) {
-			if(lo > a.get(i) - a.get(i-1)) lo = a.get(i) - a.get(i-1);
-		}
+		int lo=1, hi = a[N-1] - a[0];
 		while(lo <= hi) {
 			int mid = (lo + hi) / 2;
-//			System.out.println(" l h m " + lo + " " + hi + " " + mid);
 			if(chk(mid)) {
 				lo = mid + 1;
 			}
@@ -36,11 +32,12 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
+		a = new int[N];
 		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			a.add(Integer.parseInt(st.nextToken()));
+			a[i] = Integer.parseInt(st.nextToken());
 		}
-		a.sort(Comparator.naturalOrder());
+		Arrays.sort(a);
 		System.out.print(solve());
 	}
 }
