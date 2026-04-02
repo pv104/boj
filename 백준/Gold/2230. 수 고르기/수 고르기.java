15400a15abbe8ce67435005f89adbba40a887ab2
JunHyeok Kim <71826117+pv104@users.noreply.github.com>
2026-04-02 15:14:38 +0900

    [Gold V] Title: 수 고르기, Time: 300 ms, Memory: 28188 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+	public static int N,M;
+	public static final int INF = -1001;
+	static int[] a;
+	public static int solve() {
+		int ans=Integer.MAX_VALUE;
+		int l=0,r=0;
+		while(r != N) {
+			int x = a[l];
+			int y = a[r];
+			if(y - x == M) return M;
+			else if(y - x < M) r++;
+			else {
+				ans = Math.min(ans,y-x);
+				l++;
+			}
+		}
+		return ans;
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br;
+		br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		a = new int[N];
+		for (int i=0; i<N; i++) {
+			a[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(a);
+		System.out.print(solve());
+	}
+}
